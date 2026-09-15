7aed144b607f194e975531221828b385aeed7772
akasbhi535-stack <akasbhi535@gmail.com>
2026-09-15 15:40:37 +0530

    Add navigation functions for chat, games, profile, and home

---
+++ app.js
@@ -0,0 +1,15 @@
+function openChat() {
+    window.location.href = "chat.html";
+}
+
+function openGames() {
+    window.location.href = "games.html";
+}
+
+function openProfile() {
+    window.location.href = "profile.html";
+}
+
+function goHome() {
+    window.location.href = "index.html";
+}
